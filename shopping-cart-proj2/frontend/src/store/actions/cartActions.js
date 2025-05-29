af1035968074c8f8b502f5bab2800c3842e08def
chris Taylor <ctay456a@gmail.com>
2025-05-29 17:58:09 -0400

    Create cartActions.js

---
+++ shopping-cart-proj2/frontend/src/store/actions/cartActions.js
@@ -0,0 +1,26 @@
+// src/store/cartActions.js
+
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const UPDATE_CART_ITEM_QUANTITY = 'UPDATE_CART_ITEM_QUANTITY';
+export const SAVE_TO_CHECKOUT = 'SAVE_TO_CHECKOUT';
+
+export const addToCart = (product) => {
+  return {
+    type: ADD_TO_CART,
+    payload: { ...product, quantity: 1 }
+  };
+};
+
+export const updateCartItemQuantity = (productId, quantity) => {
+  return {
+    type: UPDATE_CART_ITEM_QUANTITY,
+    payload: { productId, quantity }
+  };
+};
+
+export const saveToCheckout = (cartItems) => {
+  return {
+    type: SAVE_TO_CHECKOUT,
+    payload: cartItems
+  };
+};
