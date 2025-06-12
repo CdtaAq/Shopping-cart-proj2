4c917b7d3a337553b2ba42ba49ea65a8e7103af5
chris Taylor <ctay456a@gmail.com>
2025-06-12 16:20:42 -0400

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,33 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Reducers
 import productReducer from './productReducer';
+import cartReducer from './cartReducer';
+import userReducer from './userReducer';
+import checkoutReducer from './checkoutReducer';
+import couponReducer from './couponReducer';
+import notificationReducer from './notificationReducer';
+import orderReducer from './orderReducer';
+import reviewReducer from './reviewReducer';
 
 const rootReducer = combineReducers({
-  products: productReducer,
+  productState: productReducer,
+  cartState: cartReducer,
+  userState: userReducer,
+  checkoutState: checkoutReducer,
+  couponState: couponReducer,
+  notificationState: notificationReducer,
+  orderState: orderReducer,
+  reviewState: reviewReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
